47fe42fc853afa00b56b2abdd728940c5dc77c43
talishesu <talishesu@code.edu.az>
2021-10-12 22:26:39 +0400

    Categories Layout Finished

---
+++ riode/Controllers/CategoriesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace riode.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BannerWithSidebar()
+        {
+            return View();
+        }
+        public IActionResult InfiniteAjaxscroll()
+        {
+            return View();
+        }
+        public IActionResult HorizontalFilter()
+        {
+            return View();
+        }
+        public IActionResult OffCanvasFilter()
+        {
+            return View();
+        }
+        public IActionResult RightToggleSidebar()
+        {
+            return View();
+        }
+        public IActionResult ThreeColumnsMode()
+        {
+            return View();
+        }
+        public IActionResult FourColumnsMode()
+        {
+            return View();
+        }
+        public IActionResult FiveColumnsMode()
+        {
+            return View();
+        }
+        public IActionResult SixColumnsMode()
+        {
+            return View();
+        }
+        public IActionResult SevenColumnsMode()
+        {
+            return View();
+        }
+        public IActionResult EightColumnsMode()
+        {
+            return View();
+        }
+        public IActionResult ListMode()
+        {
+            return View();
+        }
+    }
+}
--- riode/Controllers/HomeController.cs
+++ riode/Controllers/HomeController.cs
@@ -10,13 +10,6 @@ namespace riode.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
