10a4616b173c059f56a6d63bdc15293b5d4045a8
talishesu <talishesu@code.edu.az>
2021-10-13 00:32:04 +0400

    Blog Layout Finished

---
+++ riode/Controllers/BlogController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace riode.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Classic ()
+        {
+            return View();
+        }
+        public IActionResult Listing()
+        {
+            return View();
+        }
+        public IActionResult Grid()
+        {
+            return View();
+        }
+        public IActionResult Grid2Columns()
+        {
+            return View();
+        }
+        public IActionResult Grid3Columns()
+        {
+            return View();
+        }
+        public IActionResult Grid4Columns()
+        {
+            return View();
+        }
+        public IActionResult GridSidebar()
+        {
+            return View();
+        }
+        public IActionResult Masonry()
+        {
+            return View();
+        }
+        public IActionResult Masonry2Columns()
+        {
+            return View();
+        }
+        public IActionResult Masonry3Columns()
+        {
+            return View();
+        }
+        public IActionResult Masonry4Columns()
+        {
+            return View();
+        }
+        public IActionResult MasonrySidebar()
+        {
+            return View();
+        }
+        public IActionResult Mask()
+        {
+            return View();
+        }
+        public IActionResult BlogMaskGrid()
+        {
+            return View();
+        }
+        public IActionResult BlogMaskMasonry()
+        {
+            return View();
+        }
+        public IActionResult SinglePost()
+        {
+            return View();
+        }
+    }
+}
