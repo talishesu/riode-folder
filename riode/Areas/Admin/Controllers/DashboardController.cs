b0dbe532858cdd77cfbb46432452f1062b389abc
talishesu <talishesu@code.edu.az>
2021-10-16 13:26:35 +0400

    Admin Panel Created

---
+++ riode/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using riode.Models.DataContexts;
+using riode.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace riode.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        readonly RiodeDbContext db;
+        public DashboardController(RiodeDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            var contact = db.Contacts.ToList();   
+            return View(contact);
+        }
+    }
+}
--- riode/Startup.cs
+++ riode/Startup.cs
@@ -59,6 +59,10 @@ namespace riode
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "Areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
