7aa157875788346078c994776a8b8de4f1d26604
talishesu <talishesu@code.edu.az>
2021-11-06 11:01:42 +0400

    Comment Finishes

--- riode/Areas/Admin/Controllers/DashboardController.cs
+++ riode/Areas/Admin/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using riode.Models.DataContexts;
 using riode.Models.Entities;
 using System;
@@ -21,14 +22,56 @@ namespace riode.Areas.Admin.Controllers
             var contact = db.Contacts.ToList();   
             return View(contact);
         }
-        public IActionResult Answer()
+        public async Task<IActionResult> Answer([FromRoute]int id)
         {
-            return View();
-        }
-        public IActionResult Detail()
-        {
-            var contact = db.Contacts.ToList();
+            if (id < 1)
+            {
+                return NotFound();
+            }
+            var contact = await db.Contacts.FirstOrDefaultAsync(b => b.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
             return View(contact);
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Answer([FromRoute] int id, string answer)
+        {
+            if (ModelState.IsValid)
+            {
+                var contact = await db.Contacts.FirstOrDefaultAsync(b => b.Id == id);
+                if (contact == null)
+                {
+                    return NotFound();
+                }
+                contact.Answer = answer;
+                contact.AnswerDate = DateTime.Now;
+                contact.AnsweredByUserId = 1;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(answer);
+        }
+
+
+        public async Task<IActionResult> Detail([FromRoute] int id)
+        {
+
+            if (id < 1)
+            {
+                return NotFound();
+            }
+            var contact = await db.Contacts.FirstOrDefaultAsync(b => b.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            return View(contact);
+        }
+
     }
 }
