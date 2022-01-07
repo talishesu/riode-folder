4a6060b4c153c11f97a6cbf6f808eabaa29ea06c
Talish <talishesu@code.edu.az>
2022-01-07 11:35:20 +0400

    Updated

--- riode/Migrations/20220107073442_Init.Designer.cs
+++ riode/Migrations/20220107073442_Init.Designer.cs
@@ -10,7 +10,7 @@ using riode.Models.DataContexts;
 namespace riode.Migrations
 {
     [DbContext(typeof(RiodeDbContext))]
-    [Migration("20211202041400_Init")]
+    [Migration("20220107073442_Init")]
     partial class Init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
